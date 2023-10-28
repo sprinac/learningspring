6be92b7db7abbf3b8ac65448b8a08175db399114
sprinac <123823081+sprinac@users.noreply.github.com>
2023-10-28 13:19:51 +0100

    add learningspring initializer

---
+++ src/main/java/ie/tcd/scss/learningspring/LearningspringApplication.java
@@ -0,0 +1,13 @@
+package ie.tcd.scss.learningspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningspringApplication.class, args);
+	}
+
+}
